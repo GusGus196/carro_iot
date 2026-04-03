a0c9107dacf217f07574dfa1649d417a7379e19d
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-02 19:03:36 -0600

    feat: add alert.js module

---
+++ docs/modules/alert.js
@@ -0,0 +1,16 @@
+export function showAlert(title, message) {
+    const alert = document.getElementById('custom-alert');
+    const alertTitle = document.getElementById('alert-title');
+    const alertText = document.getElementById('alert-text');
+
+    if (alert) {
+        alertTitle.innerText = title;
+        alertText.innerText = message;
+
+        alert.classList.add('alert-visible'); // Mostramos la alerta
+
+        setTimeout(() => {
+            alert.classList.remove('alert-visible'); // Ocultamos la alerta a los 2 segundos
+        }, 2000);
+    }
+}
