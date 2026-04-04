dd1df52759494551f61d2e0dd1b33ba133c584df
EmilianoCarb <acarbajal2@ucol.mx>
2026-04-04 15:48:22 -0600

    refactor: driver, delete:variables magicas

--- lib/driver/driver.cpp
+++ lib/driver/driver.cpp
@@ -1,48 +1,55 @@
 #include "driver.h"
+const float compensacionIzquierda = 1.20f;
+const float compensacionDerecha = 1.00f;
+const float zonaMuerta = 0.1f;
+const int minPWM = 140;
+const int maxPWM = 255;
 
 void driver(float valorX, float valorY){
   // Mezcla diferencial
   float motorIzquierdo = valorY + valorX;
   float motorDerecho = valorY - valorX;
-
-  float compensacionIzquierda = 1.20;
-  float compensacionDerecha = 1.0;
-
-  motorIzquierdo = constrain(motorIzquierdo * compensacionIzquierda, -1.0, 1.0);
-  motorDerecho = constrain(motorDerecho * compensacionDerecha, -1.0, 1.0);
-
-  int valorA1 = 0, valorA2 = 0, valorB1 = 0, valorB2 = 0;
   int velocidadA = 0, velocidadB = 0;
 
+  motorIzquierdo = compensacionMotor(compensacionIzquierda,motorIzquierdo);
+  motorDerecho =  compensacionMotor(compensacionDerecha, motorDerecho);
+
   // Arranque
-  if (abs(motorIzquierdo) > 0.1) {
-    velocidadA = 140 + (abs(motorIzquierdo) * (255 - 140));
-  }
-  
-  if (abs(motorDerecho) > 0.1) {
-    velocidadB = 140 + (abs(motorDerecho) * (255 - 140));
-  }
+  velocidadA = calibracionmotor(motorIzquierdo);
+  velocidadB = calibracionmotor(motorDerecho);
 
   // Motor izquierdo (A)
-  if (motorIzquierdo > 0.1) {
-    valorA1 = 0;
-    valorA2 = velocidadA;
-  } else if (motorIzquierdo < -0.1) {
-    valorA1 = velocidadA;
-    valorA2 = 0;
+  aplicarGiroYPotencia(motorIzquierdo, velocidadA, canalA1, canalA2);
+  aplicarGiroYPotencia(motorDerecho, velocidadB, canalB1, canalB2);
+
+}
+
+int calibracionmotor(float motor){
+  motor = abs(motor);
+  if (motor > zonaMuerta) 
+  {
+    return minPWM + (motor * (maxPWM - minPWM)); 
+  }
+  return 0;
+}
+
+// Función para calcular la dirección y encender el motor
+void aplicarGiroYPotencia(float lecturaJoystick, int velocidad, int canal1, int canal2) {
+  int valor1 = 0;
+  int valor2 = 0;
+  
+  if (lecturaJoystick > zonaMuerta) {
+    valor1 = 0;
+    valor2 = velocidad; // Adelante
+  } else if (lecturaJoystick < -zonaMuerta) {
+    valor1 = velocidad; 
+    valor2 = 0;         // Reversa
   }
 
-  // Motor derecho (B)
-  if (motorDerecho > 0.1) {
-    valorB1 = 0;
-    valorB2 = velocidadB;
-  } else if (motorDerecho < -0.1) {
-    valorB1 = velocidadB;
-    valorB2 = 0;
-  }
-  
-  ledcWrite(canalA1, valorA1);
-  ledcWrite(canalA2, valorA2);
-  ledcWrite(canalB1, valorB1);
-  ledcWrite(canalB2, valorB2);
+  ledcWrite(canal1, valor1);
+  ledcWrite(canal2, valor2);
+}
+
+float compensacionMotor(float compensacion, float motor){
+  return constrain(motor * compensacion, -1.0, 1.0);
 }
--- lib/driver/driver.h
+++ lib/driver/driver.h
@@ -2,9 +2,10 @@
 #define DRIVER_H
 
 #include <Arduino.h>
-
 #include "config.h"
 
 void driver(float valorX, float valorY);
-
+int calibracionmotor(float motor);
+float compensacionMotor(float compensacion, float motor);
+void aplicarGiroYPotencia(float lecturaJoystick, int velocidad, int canal1, int canal2);
 #endif
