147388d7dc936dcdd84a58c42bfc0d62b1e71947
EmilianoCarb <acarbajal2@ucol.mx>
2026-04-04 17:42:16 -0600

    refactor: escritura pwm con un buffer estatico

--- lib/driver/driver.cpp
+++ lib/driver/driver.cpp
@@ -6,8 +6,11 @@ const int minPWM = 140;
 const int maxPWM = 255;
 const int rangoPWM = maxPWM - minPWM;
 
+//WARNING: no meter a driver.h porque va conflictuar con el "inline"
+inline void escribirPWM(int canal, int valor);
+
 void driver(float valorX, float valorY){
-  // Mezcla diferencial
+  //Diferenciales
   float motorIzquierdo = valorY + valorX;
   float motorDerecho = valorY - valorX;
   int velocidadIzq = 0, velocidadDer = 0;
@@ -33,6 +36,10 @@ int calibracionmotor(float motor){
   return 0;
 }
 
+float compensacionMotor(float compensacion, float motor){
+  return constrain(motor * compensacion, -1.0, 1.0);
+}
+
 // Función para calcular la dirección y encender el motor
 void aplicarGiroYPotencia(float lecturaJoystick, int velocidad, int canal1, int canal2) {
   int valor1 = 0;
@@ -45,11 +52,18 @@ void aplicarGiroYPotencia(float lecturaJoystick, int velocidad, int canal1, int 
     valor1 = velocidad; 
     valor2 = 0;         // Reversa
   }
-
-  ledcWrite(canal1, valor1);
-  ledcWrite(canal2, valor2);
+  escribirPWM(canal1,valor1);
+  escribirPWM(canal2,valor2);
 }
 
-float compensacionMotor(float compensacion, float motor){
-  return constrain(motor * compensacion, -1.0, 1.0);
+/*Funcion auxiliar para aplicarGiroYPotencia, verifica si hay cambio de velocidad
+para evitar reescribir inncesariamente el valor si sigue siendo constante */
+inline void escribirPWM(int canal, int valor)
+{
+  static int ultimoValor[16] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+  if (ultimoValor[canal] != valor) {
+    ledcWrite(canal, valor);
+    ultimoValor[canal] = valor;
+  }
 }
