7d4c135c0fca8199248eefabf6e171e9b34b8989
EmilianoCarb <acarbajal2@ucol.mx>
2026-04-04 14:39:42 -0600

    Refactor: funciones callback

--- lib/callback/callback.cpp
+++ lib/callback/callback.cpp
@@ -7,39 +7,80 @@ void callback(char* topic, uint8_t* payload, unsigned int length) {
 
   // Serial.print("Topic: "); Serial.print(topic); Serial.print(" Mensaje: "); Serial.println(mensajeChar);
   
-  if (strcmp(topic, "proyecto/carrito/control/joystick") == 0) {
+  mensajeTopic(topic, mensajeChar);
+  String comando = extraerComando(topic);
+  if (comando == "joystick") 
+  {
     joystick(mensajeChar); 
-  }
-
-  if(strcmp(topic, "proyecto/carrito/control/claxon") == 0) {
+  } else if(comando == "claxon") 
+  {
     claxon();
-  }
-
-  if(strcmp(topic, "proyecto/carrito/control/modo") == 0) {
+  } else if(comando == "modo")
+  {
     modo = String(mensajeChar);
     Serial.print("Modo actualizado: ");
     Serial.println(modo);
 
     sonarConfirmacion();
+  } else if(comando == "sensor") 
+  {
+    velocidadConstante = mensajeSensor(mensajeChar[0], 0.1f);
+  } else if(comando == "destino") {
+    actualizarDestino(mensajeChar);
+  }
+}
+
+/*          ==========================================
+            ==========FUNCIONES AUXILIARES============
+            ==========================================
+*/
+
+
+void mensajeModo(char* mensaje) 
+{
+    modo = String(mensaje);
+    Serial.print("Modo actualizado: ");
+    Serial.println(modo);
+}
+
+// Esta función recibe el topic completo y devuelve solo el final
+String extraerComando(const char* topicCompleto) {
+  String tema = String(topicCompleto);
+  int ultimaDiagonal = tema.lastIndexOf('/');
+  
+  // Corta y devuelve todo lo que está después de la última '/'
+  return tema.substring(ultimaDiagonal + 1);
+}
+
+//ingresamos el valor que queremos en el mensaje en el if es
+//mensajeChar[0], si es igual a '1' asignamos la velocidad deseada, sino 0.
+float mensajeSensor(char mensaje, float velocidadDeseada) {
+    return (mensaje == '1') ? velocidadDeseada : 0.00f;
   }
 
-  if(strcmp(topic, "proyecto/carrito/control/sensor") == 0) {
-    mensajeChar[0] == '1' ? velocidadConstante = 0.1 : velocidadConstante = 0.00;
-  }
+void mensajeTopic(char* topic, char* mensajeChar){
+  Serial.print("Topic: "); 
+  Serial.print(topic); 
+  Serial.print(" Mensaje: "); 
+  Serial.println(mensajeChar);
+}
+  
+void actualizarDestino(char* mensaje)
+{
+  String msg = String(mensaje);
+  int coma = msg.indexOf(','); // Busca la posición de la coma
 
-  if(strcmp(topic, "proyecto/carrito/control/destino") == 0) {
-    String msg = String(mensajeChar);
-    int coma = msg.indexOf(','); // posición de la coma, si no encuentra retorna -1
+  if (coma != -1) {
+    // Si encuentra la coma, extrae y convierte las coordenadas
+    destinoLatitud = msg.substring(0, coma).toFloat();
+    destinoLongitud = msg.substring(coma + 1).toFloat();
+    destino = true;
+    
+    Serial.print("Nuevo destino guardado -> Lat: ");
+    Serial.print(destinoLatitud, 6); // Imprime con 6 decimales de precisión
+    Serial.print(" Lon: ");
+    Serial.println(destinoLongitud, 6);
 
-    if(coma != -1) {
-      String stringLatitud = msg.substring(0, coma);
-      String stringLongitud = msg.substring(coma + 1);
-
-      destinoLatitud = stringLatitud.toFloat();
-      destinoLongitud = stringLongitud.toFloat();
-      destino = true;
-
-      sonarConfirmacion();
-    }
-  }
+    sonarConfirmacion();
+}
 }
--- lib/callback/callback.h
+++ lib/callback/callback.h
@@ -7,6 +7,11 @@
 #include "buzzer.h"
 #include "joystick.h"
 
+// Cambiamos byte por uint8_t para evitar errores de compilación
 void callback(char* topic, uint8_t* payload, unsigned int length);
-
+void mensajeModo(char* mensaje);
+String extraerComando(const char* topicCompleto);
+float mensajeSensor(char mensaje, float velocidadDeseada);
+void mensajeTopic(char* topic, char* mensajeChar);
+void actualizarDestino(char* mensaje);
 #endif
