5c4b7d81a860ff05b4f599b5db66b2ac138ce6ae
EmilianoCarb <acarbajal2@ucol.mx>
2026-04-05 13:43:14 -0600

    CALIBRAR- Solucion Base problema zigzag

--- lib/seguidor_linea/seguidor_linea.cpp
+++ lib/seguidor_linea/seguidor_linea.cpp
@@ -1,7 +1,10 @@
 #include "seguidor_linea.h"
 
+float Kp = 0.8f; // Proporcional: Qué tan brusco da el volantazo hacia la línea.
+float Kd = 0.4f; // Derivativo: El "amortiguador" que frena el giro para evitar el zigzag.
+
 void iniciarSeguidor() {
-    pinMode(pinS1, INPUT); // Extremo Izquierdo (-1.0) 33
+    pinMode(pinS1, INPUT); // Extremo Izquierdo (-1.0)
     pinMode(pinS2, INPUT); // Medio Izquierdo (-0.5)
     pinMode(pinS3, INPUT); // Centro (0.0)
     pinMode(pinS4, INPUT); // Medio Derecho (0.5)
@@ -9,6 +12,21 @@ void iniciarSeguidor() {
 }
 
 void ejecutarSeguidorLinea() {
+    float error = leerPosicionLinea();
+    
+    static float errorAnterior = 0.0f;
+
+    float P = error;                            // Proporcional: Posición actual
+    float D = error - errorAnterior;            // Derivativo: Velocidad a la que cambia el error
+
+    float correccion = (P * Kp) + (D * Kd);
+
+    errorAnterior = error;
+    
+    driver(correccion, velocidadConstante);
+}
+
+float leerPosicionLinea() {
     int s1 = !digitalRead(pinS1);
     int s2 = !digitalRead(pinS2);
     int s3 = !digitalRead(pinS3);
@@ -16,11 +34,11 @@ void ejecutarSeguidorLinea() {
     int s5 = !digitalRead(pinS5);
 
     float sumaLecturas = s1 + s2 + s3 + s4 + s5;
+    static float ultimaPosicion = 0.0f; // Memoria para no perder la línea
 
     if (sumaLecturas > 0) {
-        float x = (s1 * -1.0 + s2 * -0.5 + s4 * 0.5 + s5 * 1.0) / sumaLecturas;
-        driver(x, velocidadConstante);
-    } else {
-        driver(0, 0);
-    }
+        ultimaPosicion = (s1 * -1.0f + s2 * -0.5f + s4 * 0.5f + s5 * 1.0f) / sumaLecturas;
+    } 
+
+    return ultimaPosicion;
 }
