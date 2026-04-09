fb67d947280d55742b7ba587917535bf7a58e3d9
GusGus196 <dgusgus19@gmail.com>
2026-04-09 13:50:13 -0600

    main

--- src/config.cpp
+++ src/config.cpp
@@ -24,7 +24,7 @@ const int motorB1 = 21; // Motor derecho
 const int motorB2 = 22;
 
 // Canales PWM del ESP32
-const int canalA1 = 0;
+const int canalA1 = 0; 
 const int canalA2 = 1;
 const int canalB1 = 2;
 const int canalB2 = 3;
