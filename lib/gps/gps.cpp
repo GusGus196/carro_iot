0d3861a68f1b64120cc986612d012aed2e13cb86
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-10 16:47:29 -0600

    fix: add flag conducirHastaDestino() gps.cpp

--- lib/gps/gps.cpp
+++ lib/gps/gps.cpp
@@ -8,6 +8,7 @@ static unsigned long ultimoRumboCalculado = 0; // Controlar el calculo de rumbo 
 
 double latAnterior, lonAnterior; // Variables para punto A (anterior)
 bool primeraLecturaRealizada = false; // Bandera para omitir la primer lectura
+bool comandoEnviado = false; // Enviar una sola instrucción al driver
 
 void iniciarGPS() {
     SerialGPS.begin(9600, SERIAL_8N1, gpsRX, gpsTX);
@@ -78,13 +79,13 @@ void procesarLlegada() {
 
 void conducirHaciaDestino() {
     // Cada 5 segundos calculamos el rumbo entre el punto anterior A (lat, lon) y el actual B
-    if ( millis() - ultimoRumboCalculado > 5000) {
+    if (millis() - ultimoRumboCalculado > 5000) {
         ultimoRumboCalculado = millis();
+        comandoEnviado = false; // Reset de la bandera para el nuevo intervalo
         
         if (gps.location.isValid()) {
-            
             // Solo si la primer lectura ya fue realizada
-            if (primeraLecturaRealizada) {
+            if (latAnterior != 0 && lonAnterior != 0) {
                 /* 
                    Calculamos el rumbo basándonos en el desplazamiento real 
                    desde el punto A (anterior) hasta el punto B (actual)
@@ -100,14 +101,20 @@ void conducirHaciaDestino() {
         } else {
             driver(0.0, 0.4); // Si no hay GPS, avanzamos para buscar señal
         }
-    } else if ( millis() - ultimoRumboCalculado < 1000) {
-        if (primeraLecturaRealizada) {
-            corregirOrientacion(actualRumbo, destinoRumbo);
-        } else {
-            driver(0.0, 0.4); // Primer arranque del carro
+    } else if (millis() - ultimoRumboCalculado < 500) {
+        if (!comandoEnviado) {
+            if (primeraLecturaRealizada) {
+                corregirOrientacion(actualRumbo, destinoRumbo);
+            } else {
+                driver(0.0, 0.4); // Primer arranque del carro
+            }
+            comandoEnviado = true;
         }
     } else {
-        driver(0.0, 0.4);
+        if(comandoEnviado) {
+            driver(0.0, 0.4);
+            comandoEnviado = false;
+        }
     }
 }
 
@@ -139,7 +146,7 @@ void corregirOrientacion(double actual, double destino) {
     if (error > 180) error -= 360; // Si el error es mayor a 180, giramos a la izquierda (-)
     else if (error < -180) error += 360; // Si el error es menor a 180, giramos a la derecha (+)
     
-    float giro = (abs(error) < 30) ? 0.0 : constrain(error / 90.0, -0.4, 0.4); // Si el error es pequeño, vamos en dirección al destino
+    float giro = (abs(error) < 30) ? 0.0 : constrain(error / 90.0, -0.2, 0.2); // Si el error es pequeño, vamos en dirección al destino
     float velocidad = (abs(error) > 45) ? 0.2 : 0.3;
     
     driver(giro, velocidad);
