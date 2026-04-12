923100281cab6405e412978f6de71ae3ab2b4d2f
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-12 02:01:04 -0600

    refactor: use switch instead of if-statement main.js

--- docs/main.js
+++ docs/main.js
@@ -1,91 +1,100 @@
-import {TOPICS} from './modules/topics.js' // Topics MQTT
-import {client, enviar} from './modules/mqtt.js'; // Cliente y función enviar() MQTT
-import {iniciarJoystick, detenerJoystick} from './modules/joystick.js'; // Funciones del joystick 'modo manual'
-import {iniciarSeguidor} from './modules/seguidor.js'; // Activar y desactivar 'modo seguidor de línea'
-import {iniciarMapa, actualizarPosicion, reiniciarDestino} from './modules/gps.js'; // Funciones del mapa para 'modo navegación gps'
-import {mostrarAlerta} from './modules/alert.js'; // Alerta personalizada para navegación GPS
+import {mostrarAlerta} from "./modules/alert.js"; // Alerta personalizada
+import {iniciarMapa, actualizarPosicion, reiniciarDestino} from "./modules/gps.js"; // Funciones del mapa para modo "navegación gps"
+import {iniciarJoystick, detenerJoystick} from "./modules/joystick.js"; // Funciones del joystick para el modo "manual"
+import {client, enviar} from "./modules/mqtt.js"; // Cliente MQTT y función enviar()
+import {iniciarSeguidor} from "./modules/seguidor.js"; // Activar y desactivar el modo "seguidor de línea"
+import {TOPICS} from "./modules/topics.js" // Tópicos MQTT
 
-const modeSelect = document.getElementById('modeSelect'); // Select del modo
-const interfaceSpace = document.getElementById('interfaceSpace'); // Interfaz del modo
+const modeSelect = document.getElementById("modeSelect"); // Select del modo
+const interfaceSpace = document.getElementById("interfaceSpace"); // Interfaz del modo
 
-client.on('message', (topic, message) => {
-    // Actualizar la posición del smart car
+client.on("message", (topic, message) => {
+    // Actualizar la posición del Smart Car
     if (topic === TOPICS.ubicacion) {
-        const data = message.toString().split(','); // Mensaje recibido del smart car con su posición
-        const lat = parseFloat(data[0]); // Latitud del smart car
-        const lon = parseFloat(data[1]); // Longitud del smart car
+        const data = message.toString().split(","); // Mensaje de posición recibido del Smart Car
+        const lat = parseFloat(data[0]); // Latitud
+        const lon = parseFloat(data[1]); // Longitud
 
         if (!isNaN(lat) && !isNaN(lon)) {
             actualizarPosicion(lat, lon);
-            console.log(`${topic}: ${lat}, ${lon}`); // Comentar esta linea para dejar de mostrar mensajes de ubicación recibidos
-        };
+            
+            // Comentar la siguiente linea para dejar de mostrar mensajes de ubicación recibidos
+            console.log(`${topic}: ${lat}, ${lon}`);
+        }
     };
 
     /* 
-        Esta condición solo se cumple cuando el smart car ha llegado a su destino.
-        Se establece un radio de llegada 2-5 metros entre el smart car y el destino,
-        debido a la imprecisión que pueda causar del módulo GY-GPS6MV2
+        Esta condición solo se cumple cuando el Smart Car ha llegado a su destino.
+        Se establece un radio de llegada debido al error de precisión del módulo GY-GPS6MV2
     */
     if (topic === TOPICS.llegada) {
-        mostrarAlerta("NAVEGACIÓN GPS", "¡Se ha llegado al destino!"); // Mostramos una alerta personalizada al llegar
+        mostrarAlerta("NAVEGACIÓN GPS", "¡Se ha llegado al destino!"); // Mostramos una alerta personalizada
         reiniciarDestino(); // Función para reiniciar los valores de destino
     }
 });
 
 // Select del modo
-modeSelect.addEventListener('change', () => {
+modeSelect.addEventListener("change", () => {
     const value = modeSelect.value;
-    /*
-        value = 1, modo manual
-        value = 2, seguidor de línea
-        value = 3, navegación gps
-    */
 
-    detenerJoystick(); // Detenerlo al cambiar de modo
-    
-    if (value === "1") {
-        interfaceSpace.innerHTML = `
-            <div id="joystick-container">
-                <div id="joystick-puck"></div>
+    detenerJoystick(); // Detener al cambiar de modo (envía "0.0,0.0")
+
+    switch (value) {
+        case "1": // Modo manual
+            interfaceSpace.innerHTML = `
+                <div id="joystick-container">
+                    <div id="joystick-puck"></div>
                 </div>
+                <div class="stats">
+                    X: <span id="valX">0.00</span> | Y: <span id="valY">0.00</span>
+                </div>
+                <div class="controls">
+                    <button id="btnClaxon" class="btn-action">Claxon</button>
+                </div>
+            `;
+
+            enviar(TOPICS.modo, "control");
+            iniciarJoystick();
+            break;
+
+        case "2": // Seguidor de línea
+            interfaceSpace.innerHTML = `
+                <div class="mode-card">
+                    <button id="btnSensor" type="button" class="btn-action">Activar</button>
+                </div>
+            `;
+
+            enviar(TOPICS.modo, "linea");
+            iniciarSeguidor();
+            break;
+
+        case "3": // Navegación GPS
+            interfaceSpace.innerHTML = `
+                <div class="mode-card">
+                    <div id="mapa"></div>
                     <div class="stats">
-                        X: <span id="valX">0.00</span> | Y: <span id="valY">0.00</span>
+                        <b>Destino</b><br>
+                        Lat: <span id="latD">0.00</span> | Lon: <span id="lonD">0.00</span>
                     </div>
-                <div class="controls">
-                <button id="btnClaxon" class="btn-action">Claxon</button>
-            </div>
-        `;
-        
-        enviar(TOPICS.modo, "control");
-        iniciarJoystick();
-    } else if (value === "2") {
-        interfaceSpace.innerHTML = `
-            <div class="mode-card">
-                <button id="btnSensor" type="button" class="btn-action">Activar</button>
-            </div>
-        `;
-        
-        enviar(TOPICS.modo, "linea");
-        iniciarSeguidor();
-    } else if (value === "3") {
-        interfaceSpace.innerHTML = `
-            <div class="mode-card">
-                <div id="mapa"></div>
-                <div class="stats">
-                    <b>Destino</b><br>
-                    Lat: <span id="latD">0.00</span> | Lon: <span id="lonD">0.00</span>
+                    <div class="stats">
+                        <b>Smart Car</b><br>
+                        Lat: <span id="latC">0.00</span> | Lon: <span id="lonC">0.00</span>
+                    </div>
+                    <div class="controls">
+                        <button id="btnEnviar" class="btn-action">Enviar destino</button>
+                    </div>
                 </div>
-                <div class="stats">
-                    <b>Smart Car</b><br>
-                    Lat: <span id="latC">0.00</span> | Lon: <span id="lonC">0.00</span>
-                </div>
-                <div class="controls">
-                    <button id="btnEnviar" class="btn-action">Enviar destino</button>
-                </div>
-            </div>
-        `;
+            `;
 
-        enviar(TOPICS.modo, "gps");
-        setTimeout(iniciarMapa, 200);
-    };
+            enviar(TOPICS.modo, "gps");
+            requestAnimationFrame(() => iniciarMapa());
+            break;
+
+        default:
+            // Por si acaso (valor inválido)
+            interfaceSpace.innerHTML = `
+                <p class="placeholder-text">Selecciona un modo válido</p>
+            `;
+            break;
+    }
 });
