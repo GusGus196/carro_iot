80511ac9b6179295d32c5eb9b4e634667d4ab53f
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-13 20:01:53 -0600

    docs: document vite server configuration file

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
-export default defineConfig({
+export default defineConfig ({
   server: {
-    port: 3000,
-    host: true,
-    strictPort: true
+    port: 3000, // Puerto del servidor de desarrollo
+    host: true, // Permite el acceso desde dispositivos en la red local automáticamente al correr "npm run dev"
+    strictPort: true // Evita el cambio automático de puerto si el 3000 está en uso
+    
+    // Nota: Esta configuración es solo para desarrollo local; no afecta al build de producción
   }
-});
+})
