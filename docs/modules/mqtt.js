5d19d98dd91a9c8fcd9816bc087599a6c26bc2cd
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-17 00:41:38 -0600

    feat: create actualizarStatusMQTT function and div

--- docs/modules/mqtt.js
+++ docs/modules/mqtt.js
@@ -1,18 +1,19 @@
 import mqtt from "mqtt";
 
 import { TOPICS } from "./topics.js";
-import { mostrarAlerta } from "./alert.js";
+import { mostrarAlerta, actualizarStatusMQTT } from "./feedback.js";
 
+// Variables de entorno expuestas
 const MQTT_HOST = import.meta.env.VITE_MQTT_HOST;
 const MQTT_PORT = import.meta.env.VITE_MQTT_PORT;
 
 /*
     Selección automática de protocolo:
-    Si el puerto es 8883, 8884 o 443, usamos 'wss' (Secure WebSockets).
+    Si el puerto es 443, 8883, o 8884 usamos 'wss' (Secure WebSockets).
     De lo contrario, usamos 'ws' (WebSockets)
 */
-const protocol = (MQTT_PORT === "8883" || MQTT_PORT === "8884" || MQTT_PORT === "443") ? "wss" : "ws";
-const connectURL = `${protocol}://${MQTT_HOST}:${MQTT_PORT}`;
+const PROTOCOL = (MQTT_PORT === "443" || MQTT_PORT === "8883" || MQTT_PORT === "8884") ? "wss" : "ws";
+const connectURL = `${PROTOCOL}://${MQTT_HOST}:${MQTT_PORT}`;
 
 // Cliente MQTT
 export const client = mqtt.connect(connectURL, {
@@ -24,8 +25,8 @@ export const client = mqtt.connect(connectURL, {
 
 // Evento: conectado
 client.on("connect", () => {
-    mostrarAlerta("CLIENTE MQTT", "CONECTADO");
-    console.log(`Conexión exitosa via ${protocol} a ${MQTT_HOST}`);
+    actualizarStatusMQTT("CONECTADO", "status-online");
+    console.log(`[MQTT] Conexión establecida vía ${PROTOCOL} en ${MQTT_HOST}:${MQTT_PORT}`);
 
     // Suscripciones MQTT por defecto
     client.subscribe(TOPICS.ubicacion); // Escuchar la ubicación del Smart Car
@@ -34,34 +35,34 @@ client.on("connect", () => {
 
 // Evento: reconectar
 client.on("reconnect", () => {
-    mostrarAlerta("BROKER MQTT", "RECONECTANDO..");
+    actualizarStatusMQTT("RECONECTANDO..", "status-warning");
 });
 
 // Evento: error
 client.on("error", (err) => {
-    mostrarAlerta("BROKER MQTT", "ERROR");
-    console.error("Error MQTT:", err);
+    actualizarStatusMQTT("ERROR", "status-offline");
+    console.error("[MQTT]", err);
 });
 
 // Evento: desconectado
 client.on("offline", () => {
-    mostrarAlerta("BROKER MQTT", "DESCONECTADO");
+    actualizarStatusMQTT("DESCONECTADO", "status-offline");
 });
 
 // Función global para publicar mensajes
 export function enviar(topic, message) {
     if (!client.connected) {
-        mostrarAlerta("BROKER MQTT", "DESCONECTADO");
-        console.warn("Intento de envío fallido: cliente MQTT no conectado.");
+        actualizarStatusMQTT("DESCONECTADO", "status-offline");
+        console.warn("[PUBLISH] Fallido: cliente desconectado");
         return;
     }
 
     client.publish(topic, message, (err) => {
         if (!err) {
             // Debug de salida, comentar para producción
-            console.log(`[ENVIADO] ${topic}: ${message}`);
+            console.log(`[PUBLISH] ${topic}: ${message}`);
         } else {
-            console.error("Error al publicar:", err);
+            console.error("[PUBLISH]", err);
         }
     });
 }
