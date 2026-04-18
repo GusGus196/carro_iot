124ad83c82b3b0dae25ff2c8b5b82fd000244311
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-18 10:57:53 -0600

    fix: avoid invalid classes actualizarStatusMQTT () mqtt.js

--- docs/modules/mqtt.js
+++ docs/modules/mqtt.js
@@ -1,16 +1,16 @@
-import mqtt from "mqtt";
+import mqtt from "mqtt"; // Objeto mqtt con los métodos de la librería MQTT
 
 import { TOPICS } from "./topics.js";
 import { mostrarAlerta, actualizarStatusMQTT } from "./feedback.js";
 
-// Variables de entorno expuestas
+// Variables de entorno
 const MQTT_HOST = import.meta.env.VITE_MQTT_HOST;
 const MQTT_PORT = import.meta.env.VITE_MQTT_PORT;
 
 /*
-    Selección automática de protocolo:
-    Si el puerto es 443, 8883, o 8884 usamos 'wss' (Secure WebSockets).
-    De lo contrario, usamos 'ws' (WebSockets)
+Selección automática de protocolo:
+Si el puerto es 443, 8883, o 8884 usamos 'wss' (Secure WebSockets).
+De lo contrario, usamos 'ws' (WebSockets)
 */
 const PROTOCOL = (MQTT_PORT === "443" || MQTT_PORT === "8883" || MQTT_PORT === "8884") ? "wss" : "ws";
 const connectURL = `${PROTOCOL}://${MQTT_HOST}:${MQTT_PORT}`;
@@ -23,33 +23,33 @@ export const client = mqtt.connect(connectURL, {
     connectTimeout: 5000 // Tiempo de espera para considerar error
 })
 
-// Evento: conectado
+// Estado: conectado
 client.on("connect", () => {
     actualizarStatusMQTT("CONECTADO", "status-online");
     console.log(`[MQTT] Conexión establecida vía ${PROTOCOL} en ${MQTT_HOST}:${MQTT_PORT}`);
 
     // Suscripciones MQTT por defecto
     client.subscribe(TOPICS.ubicacion); // Escuchar la ubicación del Smart Car
-    client.subscribe(TOPICS.llegada); // Esperar notificación de llegada al destino en modo "Navegación web"
+    client.subscribe(TOPICS.llegada); // Esperar notificación de llegada al destino en modo "Navegación GPS"
 });
 
-// Evento: reconectar
-client.on("reconnect", () => {
-    actualizarStatusMQTT("RECONECTANDO..", "status-warning");
-});
-
-// Evento: error
-client.on("error", (err) => {
-    actualizarStatusMQTT("ERROR", "status-offline");
-    console.error("[MQTT]", err);
-});
-
-// Evento: desconectado
+// Estado: desconectado
 client.on("offline", () => {
     actualizarStatusMQTT("DESCONECTADO", "status-offline");
 });
 
-// Función global para publicar mensajes
+// Estado: reconectando
+client.on("reconnect", () => {
+    actualizarStatusMQTT("RECONECTANDO", "status-reconnecting");
+});
+
+// Estado: error
+client.on("error", (err) => {
+    actualizarStatusMQTT("ERROR", "status-error");
+    console.error("[MQTT]", err);
+});
+
+// Función global para publicar mensajes al broker NQTT
 export function enviar(topic, message) {
     if (!client.connected) {
         actualizarStatusMQTT("DESCONECTADO", "status-offline");
