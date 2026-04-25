ca2a65241e7ef2648b2936dd6184d9cc379f3f9e
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-04-25 15:48:38 -0600

    docs: modify feedback and html

--- docs/modules/feedback.js
+++ docs/modules/feedback.js
@@ -1,13 +1,13 @@
 let notificationTimeout;
 
-// Muestra una notificación visual (utilizada en el modo "Navegación GPS")
+// Muestra una notificación flotante de 3 segundos (usada principalmente en GPS)
 export function notificar(title, message) {
     const notification = document.getElementById("custom-notification");
     const notificationTitle = document.getElementById("notification-title");
     const notificationBody = document.getElementById("notification-body");
 
     if (notification && notificationTitle && notificationBody) {
-        clearTimeout(notificationTimeout);
+        clearTimeout(notificationTimeout); // Evita que el tiempo de la notificación anterior oculte a la nueva antes de tiempo
 
         notificationTitle.textContent = title;
         notificationBody.textContent = message;
@@ -19,7 +19,7 @@ export function notificar(title, message) {
     }
 }
 
-// Actualiza indicador visual con el estado de la comunicación MQTT (utilizada en el módulo mqttService.js)
+// Actualiza el indicador visual y el texto de estado de la conexión MQTT 
 export function actualizarEstado(message, status) {
     const dot = document.getElementById("status-dot");
     const text = document.getElementById("status-text");
@@ -38,6 +38,8 @@ export function actualizarEstado(message, status) {
         } 
 
         text.textContent = message;
+
+        // Remover clases de estado previas antes de agregar la nueva
         dot.classList.remove(...estados);
         dot.classList.add(status);
     }
