a5bbb31d5042ae166c774944345bbf2ed68af19f
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-05-01 18:21:37 -0600

    chore: update MQTT client subscriptions to new topics

--- lib/reconnect/reconnect.cpp
+++ lib/reconnect/reconnect.cpp
@@ -12,16 +12,16 @@ void reconnect() {
     if (ahora - ultimaReconexion > 5000) { 
       ultimaReconexion = ahora;
 
-      String clientId = "smartcar-esp32-" + String((uint32_t)ESP.getEfuseMac(), HEX);
+      String clientId = "smartcar-" + String((uint32_t)ESP.getEfuseMac(), HEX);
 
       if (client.connect(clientId.c_str())) {
-        client.subscribe(topic_modo);
-        client.subscribe(topic_joystick);
-        client.subscribe(topic_claxon);
-        client.subscribe(topic_seguidor);
-        client.subscribe(topic_obstaculos);
-        client.subscribe(topic_luces);
-        client.subscribe(topic_destino);
+        client.subscribe(topics.manual);
+        client.subscribe(topics.seguidor);
+        client.subscribe(topics.obstaculos);
+        client.subscribe(topics.navegacion);
+        client.subscribe(topics.modo);
+        client.subscribe(topics.claxon);
+        client.subscribe(topics.luces);
       }
     }
   }
