0a5e648085dcd619cbd514de17e8b6269ccd2720
GusGus196 <dgusgus19@gmail.com>
2026-05-02 18:50:22 -0600

    lectura ultrasonico

--- lib/obstaculos/obstaculos.cpp
+++ lib/obstaculos/obstaculos.cpp
@@ -1,6 +1,5 @@
 #include "obstaculos.h"
 
-//* Llamar a la función leerDistanciaFiltrada() 
 /*
 
 La función leerDistanciaFiltrada regresa un valor entre 0 y 400. Suele tener ruido asi que se debe ajustar, solamente tienes que hace una condicional en la que si leer 0 lo tome como 400. 
@@ -12,5 +11,14 @@ El carrito se debe de detener cuando detecte un objeto a 8cm de el, despúes de 
 void obstaculos(){
     driver(0,0.45); // Esto hace que vaya en linea recta
 
-    // Resto del código (condicionales de detección de obstaculo y giro)
+    float obstaculo = leerDistanciaFiltrada();
+
+    if(obstaculo < 7 && obstaculo > 2){ // ! Este es el rango en el que carro detecta un obstaculo
+        esquivarObstaculo();
+    }
+}
+
+void esquivarObstaculo(){
+    driver(0,0); //! Aqui se detiene
+    // El carro deberá de retroceder por lo menos hasta que la variable obstaculo tenga mas de 7cm para poder hacer el giro. También si es que ya está, se podria mandar a llamar los leds de precausión.
 }
