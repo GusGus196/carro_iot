f09393d22a6650120a507951aeba14937f0e2a53
René Núñez <221588492+rene-nunez@users.noreply.github.com>
2026-06-03 11:02:30 -0600

    chore: simplify pin config

--- src/config.cpp
+++ src/config.cpp
@@ -1,6 +1,5 @@
 #include "config.h"
 
-// Variables de configuración WIFI y MQTT
 const char* ssid = "ssid";
 const char* password = "password";
 const char* mqtt_server = "broker.hivemq.com";
@@ -11,23 +10,18 @@ const Topics topics = {
     .seguidor = "smartcar/modo/seguidor",
     .obstaculos = "smartcar/modo/obstaculos",
     .navegacion = "smartcar/modo/navegacion",
-
     .modo = "smartcar/accion/modo",
     .claxon = "smartcar/accion/claxon",
     .luces = "smartcar/accion/luces",
-
     .ubicacion = "smartcar/estado/ubicacion"
 };
 
-// Pines del driver DRV8833
-const int motorA1 = 18; // Motor izquierdo
+const int motorA1 = 18;
 const int motorA2 = 19;
-
-const int motorB1 = 13; // Motor derecho
+const int motorB1 = 13;
 const int motorB2 = 4;
 
-// Canales PWM del ESP32
-const int canalA1 = 0; 
+const int canalA1 = 0;
 const int canalA2 = 1;
 const int canalB1 = 2;
 const int canalB2 = 3;
@@ -36,49 +30,25 @@ const int canalBuzzer = 4;
 const int freq = 5000;
 const int resolucion = 8;
 
-// Pin y frecuencia del buzzer pasivo
 const int pinBuzzer = 5;
 const int freqBuzzer = 2000;
 
-// Pines del sensor ultrasónico HC-SR04
 const int trig = 14;
 const int echo = 23;
 
-// Pines del array de 5 sensores reflectivos TCRT5000
 const int pinS1 = 32;
 const int pinS2 = 33;
 const int pinS3 = 25;
 const int pinS4 = 26;
 const int pinS5 = 27;
 
-float velocidadConstante = 0.00; // Velocidad constante
-float momentum = 0;
-
-// Sensores de velocidad
 const int sensorVelDer = 34;
 const int sensorVelIzq = 35;
 
-// Pines del módulo GPS GY-GPS6MV2
 const int gpsRX = 16;
 const int gpsTX = 17;
 
-// Variables del modo 'navegación GPS'
-double latDestino = 0.0;
-double lonDestino = 0.0;
-bool estadoNav = false;
-
-const char* accionNav = ""; // Iniciar, detener o reanudar
-
-double errorRumbo = 0.0;
-
-// Variables de estado
-String modo = "indefinido"; // Modo seleccionado (control, linea o gps)
-unsigned long ultimaVezRecibido = 0; // Última vez recibido un mensaje MQTT para joystick
-
-const char* tipo = "";
-
-// Pines LEDS
-const ConfigLuces lucesConf { 
+const ConfigLuces lucesConf {
     .pinR = 0,
     .pinG = 1,
     .pinB = 2,
@@ -90,8 +60,7 @@ const ConfigLuces lucesConf {
     .colorSeguidor = {0, 1, 0},
     .colorObstaculos = {1, 0, 0},
     .colorNavegacion = {1, 1, 0},
-    .colorNull = {0,0,0}
+    .colorNull = {0, 0, 0}
 };
 
-unsigned long ultimaVezLuces = 0;
-const unsigned long timeoutLuces = 3000; 
+const unsigned long timeoutLuces = 3000;
